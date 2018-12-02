ac9d5d4ff16f9d8c4d43063e177dbda4392e4e53
Vladimir.Zaharov <zaxar219@mail.ru>
2018-12-02 15:06:10 +0300

    dz-portfolio-val

--- docs/js/main.js
+++ docs/js/main.js
@@ -18,28 +18,26 @@ $(document).ready(function() {
                 }
             }
     });
-	// End of FancyBox - galery
-	
-	
 
-//	(function() {
-//
-//	  "use strict";
-//
-//	  var toggles = document.querySelectorAll(".cmn-toggle-switch");
-//
-//	  for (var i = toggles.length - 1; i >= 0; i--) {
-//		var toggle = toggles[i];
-//		toggleHandler(toggle);
-//	  };
-//
-//	  function toggleHandler(toggle) {
-//		toggle.addEventListener( "click", function(e) {
-//		  e.preventDefault();
-//		  (this.classList.contains("active") === true) ? this.classList.remove("active") : this.classList.add("active");
-//		});
-//	  }
-//
-//	})();
-
+	// jQuery Validate JS
+	$("#contact-form").validate({
+		rules: {
+			name: { required: true },
+			email: { required: true, email:true },
+//			skype: { required: true },
+//			phone: { required: true },
+			massage: { required: true }
+		},
+		messages: {
+			name: "Пожалуйста, введите свое имя",
+			email: {
+				required: "Пожалуйста, введите свой email",
+				email: "Email адрес должен быть в формате name@domain.com . Возможно вы ввелиemail с ошибкой."
+			},
+			massage: "Пожалуйста, введите текст сообщения",
+			
+		}
+		
+	})
+	
 }); 
