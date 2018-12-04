dd313c80364c48f1f5ebc6ae630084aaa42b6b88
Vladimir.Zaharov <zaxar219@mail.ru>
2018-12-04 06:08:10 +0300

    dz-porfolio-final

--- docs/js/main.js
+++ docs/js/main.js
@@ -9,6 +9,22 @@ $(document).ready(function() {
 	// MixItUp - Фильтрация работ в портфолио
 	$('#portfolio-projects').mixItUp();
 	
+	
+	
+	//Рамка фильтра работ
+	
+	var filter_btn = $ ('.filter-block__button ');
+  var class_active = 'filter-block__button--active';
+
+  // Фильтр работ
+  filter_btn.on('click', function (e) {
+      e.preventDefault ();
+      $(filter_btn).removeClass(class_active);
+      $(this).addClass(class_active);
+  });
+	
+	
+
 	// FancyBox - galery
 	$(".fancybox").fancybox({
 			// Default - with fix from scroll to top
@@ -36,8 +52,48 @@ $(document).ready(function() {
 			},
 			massage: "Пожалуйста, введите текст сообщения",
 			
+		},
+		
+		submitHandler: function(form) {
+		  ajaxFormSubmit();
 		}
 		
 	})
+	
+	
+	// Функция AJAX запрса на сервер
+	function ajaxFormSubmit(){
+		var string = $("#contact-form").serialize(); // Соханяем данные введенные в форму в строку. 
+
+		// Формируем ajax запрос
+		$.ajax({
+			type: "POST", // Тип запроса - POST
+			url: "php/mail.php", // Куда отправляем запрос
+			data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+			
+			// Функция если все прошло успешно
+			success: function(html){
+				$("#contact-form").slideUp(800);
+				$('#answer').html(html);
+			}
+		});
+
+		// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+		return false; 
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
 	
 }); 
